c2181efbf40aa821b468f3c01bbacab0432c97db
annisha <channisha03@gmail.com>
2019-02-01 17:14:07 +0700

    Updates

---
+++ precache-manifest.27cc5457724f70081376a83774be4435.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/homepage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c9f2de54d44bb9049b00",
+    "url": "/homepage/static/js/runtime~main.c9f2de54.js"
+  },
+  {
+    "revision": "7938e1452eba5171a901",
+    "url": "/homepage/static/js/main.7938e145.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/homepage/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "7938e1452eba5171a901",
+    "url": "/homepage/static/css/main.bf889a5e.chunk.css"
+  },
+  {
+    "revision": "a854cc4fde37821912356e3588a6cbf1",
+    "url": "/homepage/index.html"
+  }
+];
